783f36b8ed6f50ea9a7ccd40be404db6827aa586
hamiltino <mathew220@hotmail.com>
2017-12-22 18:57:22 +1100

    Add files via upload

---
+++ pdfCounter/src/main/java/App.java
@@ -0,0 +1,50 @@
+package pdfCounter;
+
+import java.io.File;
+
+import java.io.FileFilter;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Iterator;
+import org.apache.commons.io.FileUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+public class App {
+
+    public static void main(String args[]) {
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        File root = new File(s);
+        int count = 0;
+        try {
+            boolean recursive = true;
+
+            Collection files = FileUtils.listFiles(root, null, recursive);
+
+            for (Iterator iterator = files.iterator(); iterator.hasNext(); ) {
+                File file = (File) iterator.next();
+                if (file.getName().endsWith(".pdf")) {
+                    System.out.println(file.getAbsolutePath());
+                    PDDocument doc = PDDocument.load(new File(file.getAbsolutePath()));
+                    count = doc.getNumberOfPages() + count;
+                }
+
+                try (PrintStream out = new PrintStream(new FileOutputStream("NumberOfPages.txt"))) {
+                    out.print(count);
+                }
+            }
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+
+
+
+    }
+}
+
+
+
+
